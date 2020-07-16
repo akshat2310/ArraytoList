10667d1125b50a7c5690ff20f16cc13505987571
akshat2310 <56287654+akshat2310@users.noreply.github.com>
2020-07-17 00:21:22 +0530

    Add files via upload

---
+++ ArraytoList.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class ArraytoList {
+    public static void main(String[] args) {
+
+        String[] array = {"a", "b"};
+        List<String> list = Arrays.asList(array);
+
+        System.out.println(list);
+
+    }
+}
